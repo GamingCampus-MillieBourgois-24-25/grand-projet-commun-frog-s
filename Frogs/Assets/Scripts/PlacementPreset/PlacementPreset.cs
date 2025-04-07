8fa5561fd8b62dbb5127639f367ba220d40c232e
unknown <kdifallah@gamingcampus.local>
2025-04-07 09:59:29 +0200

    Plance batiment when enough money

--- Frogs/Assets/Scripts/PlacementPreset/PlacementPreset.cs
+++ Frogs/Assets/Scripts/PlacementPreset/PlacementPreset.cs
@@ -6,9 +6,26 @@ using UnityEngine.UI;
 public class PlacementPreset : MonoBehaviour
 {
     [SerializeField] private GameObject BuildingToPlace;
-    
+
+    private GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    void Update()
+    {
+        if(gameManager.GetMoney() >= 30){
+            gameObject.GetComponent<Canvas>().enabled = true;
+        }
+        else{
+            gameObject.GetComponent<Canvas>().enabled = false;
+        }
+    }
+
     public void PlaceBuilding(){
-        Debug.Log("Clique");
+        gameManager.RemoveMoney(30);
         Instantiate(BuildingToPlace, gameObject.transform.position, BuildingToPlace.transform.rotation);
         Destroy(gameObject);
     }
